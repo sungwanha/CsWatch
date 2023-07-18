315917235d13308aa957ae837fb537dfcd6fc1ea
Sungwan Ha <124125296+sungwanha@users.noreply.github.com>
2023-07-18 22:03:48 +0900

    Add files via upload

---
+++ Watch/ucPanel/AlarmPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Watch.ucPanel
+{
+    public partial class AlarmPanel : UserControl
+    {
+        public AlarmPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
