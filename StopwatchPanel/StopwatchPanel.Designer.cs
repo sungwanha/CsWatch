bec71cb129a5fed499109d926e755250600b3a61
Sungwan Ha <124125296+sungwanha@users.noreply.github.com>
2023-07-19 14:08:34 +0900

    Add files via upload

---
+++ StopwatchPanel/StopwatchPanel.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Watch.ucPanel
+{
+    partial class StopwatchPanel
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(17, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Stopwatch";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.MenuText;
+            panel1.Location = new Point(17, 25);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 2);
+            panel1.TabIndex = 1;
+            // 
+            // StopwatchPanel
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "StopwatchPanel";
+            Size = new Size(297, 274);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+    }
+}
