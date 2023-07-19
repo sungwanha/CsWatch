6c98d518c4ee36a264c030d660c6450b17e6ac8c
Sungwan Ha <124125296+sungwanha@users.noreply.github.com>
2023-07-19 14:25:00 +0900

    Add files via upload

---
+++ Watch/ucPanel/AlarmPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Watch.ucPanel
+{
+    public partial class AlarmPanel : UserControl
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeft,
+            int nTop,
+            int nRight,
+            int nBottom,
+            int nWidthEllipse,
+            int nHeightEllipse);            
+        
+        public AlarmPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void AlarmPanel_Load(object sender, EventArgs e)
+        {
+            button1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 30, 30));
+        }
+    }
+}
